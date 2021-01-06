8e03f7d1a741934db9cd428675e387035cfba056
Bounaix Vincent <bounaixvincent@gmail.com>
2021-01-06 17:38:34 +0100

    feat: creates fractal in black and white then saves it as png

--- src/main/java/org/example/Mandelbrot.java
+++ src/main/java/org/example/Mandelbrot.java
@@ -1,7 +1,9 @@
 package org.example;
 
-import java.awt.Graphics;
+import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class Mandelbrot extends JFrame {
@@ -15,7 +17,7 @@ public class Mandelbrot extends JFrame {
         setBounds(100, 100, 800, 600);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_RGB);
+        I = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_BYTE_BINARY);
         for (int y = 0; y < getHeight(); y++) {
             for (int x = 0; x < getWidth(); x++) {
                 zx = zy = 0;
@@ -31,11 +33,41 @@ public class Mandelbrot extends JFrame {
                 I.setRGB(x, y, iter | (iter << 8));
             }
         }
+        invertBlackAndWhite(I);
+        saveFileAsJng(I);
     }
 
     @Override
     public void paint(Graphics g) {
         g.drawImage(I, 0, 0, this);
+    }
+
+    public void saveFileAsJng(BufferedImage bufferedImage) {
+        try {
+            BufferedImage bi = bufferedImage;
+            File outputFile = new File("fractal.jpg");
+            ImageIO.write(bi, "jpg", outputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public BufferedImage invertBlackAndWhite(BufferedImage image) {
+        BufferedImage imageOut = image;
+        for (int x = 0; x < image.getWidth(); x++) {
+            for (int y = 0; y < image.getHeight(); y++) {
+                Color c = new Color(image.getRGB(x, y));
+                //invert white into black
+                if (c.equals(Color.white)) {
+                    I.setRGB(x, y, Color.black.getRGB());
+                } else {
+                    I.setRGB(x, y, Color.white.getRGB());
+                }
+
+            }
+        }
+
+        return imageOut;
     }
 
     public static void main(String[] args) {
