215cf032d909bd52c4ea214a90752531d88b0541
AgasnotPro <agasnotpro@gmail.com>
2021-01-11 17:01:46 +0100

    begining

---
+++ src/main/java/org/example/OurExecutorService.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.concurrent.Executor;
+
+public class OurExecutorService implements Executor {
+    @Override
+    public void execute(Runnable command) {
+
+    }
+}
