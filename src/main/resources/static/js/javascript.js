bcf8e87b2a64270c284259139aeb789e7d065697
Nono <arnaud.chretien@outlook.com>
2021-01-12 10:00:56 +0100

    julia is rdy

--- src/main/resources/static/js/javascript.js
+++ src/main/resources/static/js/javascript.js
@@ -1,10 +1,10 @@
 function getJulia() {
-    return fetch("/getImage?direction=none&name=julia")
+    return fetch("/getImage?direction=none&type=julia")
     .then(response => response.blob())
     .then(data => document.getElementById("FractalImage").src=URL.createObjectURL(data));
 }
 function getImage(direction) {
-    return fetch('/getImage'+'?direction='+direction+"&name=mandel")
+    return fetch('/getImage'+'?direction='+direction+"&type=mandel")
     .then(response => response.blob())
     .then(data => document.getElementById("FractalImage").src=URL.createObjectURL(data));
 }
