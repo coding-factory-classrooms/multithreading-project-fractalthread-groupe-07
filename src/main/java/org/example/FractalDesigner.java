ce91146128239f29870f9c47cc4d156dafdc734d
AgasnotPro <agasnotpro@gmail.com>
2021-01-13 16:41:08 +0100

    easter egg ok

--- src/main/java/org/example/FractalDesigner.java
+++ src/main/java/org/example/FractalDesigner.java
@@ -1,7 +1,11 @@
 package org.example;
 
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.*;
 
@@ -9,6 +13,7 @@ public class FractalDesigner {
     private ArrayList<Future<ImageFraction>> futures;
     private Fractal fractal;
     private ExecutorService threadPool;
+    private static final String EASTER_EGG_PATH = "src/main/resources/static/img/easter_egg_equipe.png";
     public FractalDesigner(Fractal fractal, ExecutorService threadPool) {
         this.fractal = fractal;
         this.threadPool = threadPool;
@@ -22,6 +27,7 @@ public class FractalDesigner {
             createFractions(linesByChunk);
             threadPool.shutdown();
             mergeFractions();
+            addEasterEggOrNot();
         }
         else {
             System.out.println("draw julia");
@@ -42,6 +48,27 @@ public class FractalDesigner {
                 g.drawImage(futures.get(i).get().fraction, 0, futures.get(i).get().fraction.getHeight() * i, null);
             }
         }  catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        }
+        g.dispose();
+        fractal.saveFileAsJpg(fractal.getImage());
+    }
+
+    private void addEasterEggOrNot() {
+        if (fractal.getPosX() == 2000 && fractal.getPosY() == -2500) {
+            System.out.println("display easter egg");
+            drawEasterEgg();
+        }
+    }
+
+    private void drawEasterEgg() {
+        Graphics g = fractal.getImage().getGraphics();
+        try {
+            BufferedImage original = ImageIO.read(new File(fractal.getImagePath()));
+            BufferedImage overlay = ImageIO.read(new File(EASTER_EGG_PATH));
+            g.drawImage(original, 0, 0, null);
+            g.drawImage(overlay, fractal.getHorizontalSide()/4, fractal.getVerticalSide()/4, null);
+        } catch (IOException e) {
             e.printStackTrace();
         }
         g.dispose();
