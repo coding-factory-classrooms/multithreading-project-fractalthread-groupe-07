a9fca73952fa6ce88b9ebc6862f3a1b214fd23eb
Nono <arnaud.chretien@outlook.com>
2021-01-14 11:14:10 +0100

    test Save Image

--- src/test/java/FractalTests.java
+++ src/test/java/FractalTests.java
@@ -4,6 +4,13 @@ import org.example.Mandelbrot;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class FractalTests {
     @Test
     public void test_should_return_mandelbrot() {
@@ -30,9 +37,32 @@ public class FractalTests {
         Assert.assertEquals(fractal.getName(), "julia");
     }
     @Test
-    public void test_should_save_and_retrieve_img() {
-        // SUCH WOW!
-        Assert.assertEquals(4, 2 + 2);
+    public void test_should_save_image() {
+        // GIVEN //
+        Mandelbrot mandelbrot = new Mandelbrot();
+        BufferedImage image = new BufferedImage(256, 256,
+                BufferedImage.TYPE_INT_RGB);
+        String path = "src/main/resources/static/img/test.jpg";
+        mandelbrot.setImagePath(path);
+        // WHEN //
+        mandelbrot.saveFileAsJpg(image);
+
+        // THEN //
+        BufferedImage imageSaved = null;
+        try {
+            imageSaved = ImageIO.read(new File(path));
+        } catch (IOException e) {
+            Assert.assertTrue(false);
+        }
+        Assert.assertNotNull(imageSaved);
+
+        //FLUSH
+        try {
+            Files.deleteIfExists(Path.of(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
     @Test
     public void test_should_handle_directions() {
